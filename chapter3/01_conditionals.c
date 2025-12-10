10934a751e490de30530184f109d655b42e33b87
mdruhulamin7 <ruhulthisis@gmail.com>
2025-12-10 18:42:19 +0600

    Chapter 3 Condional statement

---
+++ chapter3/01_conditionals.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 30;
+
+    if (age > 20)
+    {
+        printf("You age is greater then 20\n");
+    }
+    if  (age % 10 == 0)
+    {
+        printf("You age is greater then 10");
+    }
+  
+    return 0;
+}
---
+++ chapter3/02_if_else.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 30;
+
+    if (age > 20)
+    {
+        printf("You age is greater then 20\n");
+    }
+    else{
+        printf("You age is not greater then 20");
+    }
+  
+    return 0;
+}

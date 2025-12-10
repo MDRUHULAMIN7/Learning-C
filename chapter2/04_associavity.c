39d11e83c3d8b64269e42701bde7e765119a7ec5
mdruhulamin7 <ruhulthisis@gmail.com>
2025-12-10 17:50:16 +0600

    type casting

--- chapter2/04_associavity.c
+++ chapter2/04_associavity.c
@@ -3,8 +3,26 @@
 int main(){
     int a ,b ,c ;
      a = 3 ;
-     b =6 ;
+     b = 6 ;
      c = 9 ;
-     printf("the value is %d", a * b / c + 6);
+     printf("the value is %d\n", a * b / c + 6);
+     printf("the value is %d",3*b/2*c + 6*a); // ans will 99
+     /* The following table lists the operator priority in C
+ Priority Operators
+
+1st * / %
+2nd + -
+3rd =
+
+sei hisabe first a * / % ar kaj hbe tarpor + - ar kaj hbe then = ar kaj hbe 
+
+3*b/2*c + 6*a 
+3*b/2*c + 18 
+18/2*c + 18 
+9*c + 18 
+81 + 18 
+99 
+hurray ans is write 
+*/
     return 0;
 }
---
+++ chapter2/05_type_casting.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    int n  = 44 ;
+    int m = 32.33;
+    n = (int) m; // convert the data type into int
+
+    printf("%d\n",n);
+    return 0;
+}

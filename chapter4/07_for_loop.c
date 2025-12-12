bf48ff1d641abfd010fbd4a31271d8eb46d622ae
mdruhulamin7 <ruhulthisis@gmail.com>
2025-12-12 09:46:52 +0600

    for loop

---
+++ chapter4/07_for_loop.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int n = 6;
+    for (int i = 0; i < n; i++)
+    {
+        printf("the value of i is %d\n", i);
+    }
+    return 0;
+}
+/*
+Initialize → Setting a loop counter to an initial value.
+ Test → Checking a condition.
+Increment → Updating the loop counter.*/

c7e78581eb137853aca21600a9d9536a5bf40848
mdruhulamin7 <ruhulthisis@gmail.com>
2025-12-12 15:28:16 +0600

    Start Problem Set of Chapter 4

--- chapter4/10_break_continue.c
+++ chapter4/10_break_continue.c
@@ -4,9 +4,10 @@ int main()
 {
     for (int i = 0; i <= 15; i++)
     {
-        if(i==5){
-            //break; // exit the loop !
-            continue; // exit the iteration now mane holo ai condition ar sathe mila gele oi iteration mae block excute hbe na scip korbe okhane theke abar upure chole jabe 
+        if (i == 5)
+        {
+            // break; // exit the loop !
+            continue; // exit the iteration now mane holo ai condition ar sathe mila gele oi iteration mae block excute hbe na scip korbe okhane theke abar upure chole jabe
         }
         printf("i is %d\n", i);
     }
